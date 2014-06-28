5369f16787132e08af1fcd42937f4e08e2ec1417
jimmyleeking <l02n20l@qq.com>
2014-06-28 10:08:02 +0800

    Add Store Engine

---
+++ Linker/Linker/Models/LNStoreEngine.h
@@ -0,0 +1,13 @@
+//
+//  LNStoreEngine.h
+//  Linker
+//
+//  Created by jimmylee on 6/28/14.
+//  Copyright (c) 2014 Handos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LNStoreEngine : NSObject
+
+@end
---
+++ Linker/Linker/Models/LNStoreEngine.m
@@ -0,0 +1,13 @@
+//
+//  LNStoreEngine.m
+//  Linker
+//
+//  Created by jimmylee on 6/28/14.
+//  Copyright (c) 2014 Handos. All rights reserved.
+//
+
+#import "LNStoreEngine.h"
+
+@implementation LNStoreEngine
+
+@end
